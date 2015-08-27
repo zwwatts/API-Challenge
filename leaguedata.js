a424ff63a1628367e3d83a8f256ae0f7007b325e
Joel Keyser <keyserj@uwplatt.edu>
2015-08-27 11:52:56 -0500

    leaguedata.js correctly grabs league data - not tested with html

---
+++ leaguedata.js
@@ -0,0 +1,117 @@
+//'requires' may have to be converted to <script src="">?
+
+//Server has all of the functions from the LolApi (leaguejs),
+//except it is already initialized with my key.
+var server = require("./server.js");
+var fs = require('fs');
+
+var region = 'na';
+var matches = {};
+matches.before = {};
+matches.after = {};
+
+function setMatchesBeforeRanked(region) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./5.11/RANKED_SOLO/' + region + '.json', 'utf8', function(err,data) {
+            if (err) {
+                reject(Error("Failed matches.before.ranked"));
+            }
+            resolve(data);
+        });
+    });
+}
+
+function setMatchesAfterRanked(region) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./5.14/RANKED_SOLO/' + region + '.json', 'utf8', function(err,data) {
+            if (err) {
+                reject(Error("Failed matches.before.ranked"));
+            }
+            resolve(data);
+        });
+    });
+}
+
+function setChamps() {
+    return new Promise(function(resolve, reject) {
+        server.LolApi.Static.getChampionList(true, function(err, champsRetrieved) {
+            resolve(champsRetrieved);
+        });
+    });
+}
+
+function setItems() {
+    return new Promise(function(resolve, reject) {
+        server.LolApi.Static.getItemList(true, function(err, items) {
+            if (err) {
+                reject(Error("Failed Items"));
+            }
+            resolve(items);
+        });
+    });
+}
+
+function setSummoner() {
+    return new Promise(function(resolve, reject) {
+        server.LolApi.Summoner.getByName('Foe One Flame', function(err, summonerRetrieved) {
+            if(!err) {
+                resolve(summonerRetrieved);
+            }
+            else {
+                reject(Error("Failed Summoner"));
+            }
+        });
+    })
+}
+
+//Nested loop to make sure all promises are fulfilled.
+//All data will be accessed at the inner-most nest.
+//Starts with the promise to set champs.
+setChamps().then(function(result) {
+    champs = result;
+    
+    //Promise to set items
+    setItems().then(function(result) {
+        items = result;
+        
+        var apItemIndexes = [3115, 3116, 3135, 3136, 3151, 3165,
+                             3174, 3001, 3003, 3027, 3089];
+        var apItems = [];
+        for (var i in apItemIndexes) {
+            apItems.push(items.data[apItemIndexes[i]].name);
+        }
+        
+        //Promise to set summoner
+        setSummoner().then(function(result) {
+            summoner = result;
+            
+            //Promise to set matches.before.ranked
+            setMatchesBeforeRanked(region).then(function(result) {
+                matches.before.ranked = result;
+                
+                //Promise to set matches.after.ranked
+                setMatchesAfterRanked(region).then(function(result) {
+                    matches.after.ranked = result;
+                    
+                    //Do something with all of the data.
+                    //Basically here should be functions that
+                    // take in the data as parameters.
+                    //The functions should be defined globally
+                    // (outside of the nests).
+                    
+                    
+                }).catch(function(err) {
+                    
+                });
+            }).catch(function(err) {
+                
+            });
+        }).catch(function(err) {
+            
+        });
+    }).catch(function(err) {
+        
+    });
+}).catch(function(err) {
+    
+});
--- server.js
+++ server.js
@@ -3,31 +3,17 @@ var LolApi = require('leagueapi');
 // MY KEY!! no stealerino
 LolApi.init('0b569de1-9c38-4e8b-a802-a6032910689c', 'na');
 
-LolApi.Static.getChampionList(true, function(err, champs) {
-            console.log(champs.data);
-});
 
-var currentSummoner;
-LolApi.Summoner.getByName('Foe One Flame', function(err, summoner) {
-    if(!err) {
-        console.log(summoner);
-        console.log(summoner.id);
-        console.log(summoner.name);
-        console.log(summoner.summonerLevel);
-    }
-});
+var toExport = {};
+toExport.LolApi = LolApi;
 
-//The wrapper also accepts promises:
-LolApi.Summoner.getByName('bowling')
-.then(function (summoner) {
-    console.log(summoner);
-});
+module.exports = toExport;
 
-if (currentSummoner) {
-    console.log(currentSummoner.id);
-    LolApi.getMatchHistory(currentSummoner.id, 'na', function(err, history) {
+/*if (summoner) {
+    console.log(summoner.id);
+    LolApi.getMatchHistory(summoner.id, 'na', function(err, history) {
         if(!err) {
             console.log(history);
         }
     });
-}
+}*/
